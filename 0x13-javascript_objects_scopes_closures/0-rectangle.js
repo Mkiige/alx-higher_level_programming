618dbf79012f919023d6fc61edfcb1bbaf89689e
Mkiige <mkiige77@gmail.com>
2022-12-20 19:44:36 +0300

    upd

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,15 +1,2 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-
-  get perimeter() {
-    return 2 * (this.width + this.height);
-  }
-
-  get area() {
-    return this.width * this.height;
-  }
-}
+module.exports = class Rectangle {};
